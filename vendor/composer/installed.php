3e7e0cc4813929ddc3d946ccc53b1024c7b78c4e
Nicolas Guerra Rocha <nicolas@tectel.com.uy>
2024-03-05 22:23:31 -0300

    Mercado Pago v3.0.3 installed

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nagruy/mp-ecommerce-php',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'e3aedbfa369010f518359439ddcfb5c7c45a088c',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mercadopago/dx-php' => array(
+            'pretty_version' => '3.0.3',
+            'version' => '3.0.3.0',
+            'reference' => '869313f6218741ce98a92b9d35c545b7193ca353',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mercadopago/dx-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'nagruy/mp-ecommerce-php' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'e3aedbfa369010f518359439ddcfb5c7c45a088c',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
